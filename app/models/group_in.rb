626241339e5678d219ebe6c3f78c8c3cc9e5d9b1
Diego Martinez <55361667+dmartinezm@users.noreply.github.com>
2020-01-10 11:11:44 -0500

    made some changes to a new relationship- GroupIn, but deleted it

---
+++ app/models/group_in.rb
@@ -0,0 +1,4 @@
+class GroupIn < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :group_ins
   resources :friendships
   resources :expenses
   resources :groups
